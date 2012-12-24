0caf90cdd406e00fa450531454326cccdef306cf
Zhang Ding <zhangdingccc@gmail.com>
2012-12-24 21:40:09 +0800

    Memory warning log test

--- proj.ios/RootViewController.mm
+++ proj.ios/RootViewController.mm
@@ -76,6 +76,8 @@
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
     
+    NSLog(@"Memory warning!");
+    
     // Release any cached data, images, etc that aren't in use.
 }
 
